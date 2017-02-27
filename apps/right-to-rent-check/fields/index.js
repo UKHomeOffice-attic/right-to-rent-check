6cd68e5894c51640d5180c6588ae225ad921f0cb
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2017-02-27 11:02:23 +0000

    Add the tenant details page (#6)
    * * Add the request-property page with fields
    * Add h1 css
    * Add hof-transpiler, nodemon, npm-run-all, browsify
    * Move npm-sass to general dependency rather than devDependency
    * Add the watch app, watch js, watch scss
    
    https://jira.digital.homeoffice.gov.uk/browse/LCS-202
    
    * * Add forking to request-property page
    * Add placeholder header for request-current-property-address
    * Add placeholder header for request-tenancy-start
    * Change journey header to Home Office right to rent check
    * Remove the duplicate browsify script and use build:js instead in the watch
    * Remove browsify dependency doesn't seem to be needed
    
    * * sort out the spacing for field.json translations file
    
    * updated scripts in package.json start runs more straightforward, added a start:dev script and simplified the watch:app
    
    * * Adding the tenant details page
    * Getting the country selector to display
    * WIP for the datecomponent to work
    
    * * bump hof-bootstrap to use the new date component
    * Remove hof-controllers
    * watch for js changes
    
    Tenant-details
    * change url
    * add dob
    * add translations
    * adding styling
    * change url names in steps
    
    * Request-property
    * Add a legend for accessibility reasons
    * Remove header as hof adds header if necessary
    
    Tenant-details
    * Date-component: remove parts as it works for GDS date format by default. Parts only needed if not GDS format

--- apps/right-to-rent-check/fields/index.js
+++ apps/right-to-rent-check/fields/index.js
@@ -1,8 +1,29 @@
 'use strict';
+const dateComponent = require('hof-component-date');
 
 module.exports = {
-  name: {
+  'living-status': {
+    mixin: 'radio-group',
+    validate: 'required',
+    legend: {
+      className: 'visuallyhidden'
+    },
+    options: [
+      'yes',
+      'no'
+    ]
+  },
+  'tenant-name': {
     mixin: 'input-text',
     validate: 'required'
-  }
+  },
+  'tenant-country': {
+    mixin: 'select',
+    validate: 'required',
+    className: ['typeahead', 'js-hidden'],
+    options: [''].concat(require('homeoffice-countries').allCountries)
+  },
+  'tenant-dob': dateComponent('tenant-dob', {
+    validate: ['required', 'date', 'before']
+  }),
 };
--- apps/right-to-rent-check/index.js
+++ apps/right-to-rent-check/index.js
@@ -1,21 +1,44 @@
 'use strict';
 
-const controllers = require('hof-controllers');
+const dateComponent = require('hof-component-date');
 
 module.exports = {
   name: 'right-to-rent-check',
   baseUrl: '/right-to-rent-check',
+  params: '/:action?/:id?',
   steps: {
-    '/': {
-      controller: controllers.start,
-      next: '/name'
+    '/request-property': {
+      fields: ['living-status'],
+      next: '/request-tenancy-start',
+      forks: [{
+        target: '/request-current-property-address',
+        condition: {
+          field: 'living-status',
+          value: 'no'
+        }
+      }]
     },
-    '/name': {
-      fields: ['name'],
+    '/request-tenancy-start': {
+      next: '/tenant-details'
+    },
+    '/request-current-property-address': {
+      next: '/request-current-property-address-select'
+    },
+    '/request-current-property-address-select': {
+      next: '/tenant-details'
+    },
+    '/tenant-details': {
+      fields: [
+        'tenant-name',
+        'tenant-dob',
+        'tenant-country'
+      ],
+      next: '/tenant-additional-details'
+    },
+    '/tenant-additional-details': {
       next: '/confirm'
     },
     '/confirm': {
-      controller: require('hof-confirm-controller'),
       next: '/complete'
     },
     '/complete': {}

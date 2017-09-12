df19d5bd2af44dee6788b6a6c472f331ed510642
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2017-09-12 17:26:49 +0100

    check-not-needed-link move html link out of the translations and keep just the text

--- apps/right-to-rent-check/acceptance/features/check-not-needed-date.js
+++ apps/right-to-rent-check/acceptance/features/check-not-needed-date.js
@@ -4,16 +4,11 @@ const steps = require('../../');
 
 Feature('Given I am on /check-not-needed-date');
 
-Before((
+Scenario('When I click on the back to guidance link I am taken to https://www.gov.uk/check-tenant-right-to-rent-documents', (
   I,
   checkNotNeededDatePage
 ) => {
   I.visitPage(checkNotNeededDatePage, steps);
-});
-
-Scenario('When I click on the back to guidance link I am taken to https://www.gov.uk/check-tenant-right-to-rent-documents', (
-  I
-) => {
   I.click('#content a');
-  I.seeInCurrentUrl('www.gov.uk/check-tenant-right-to-rent-documents');
+  I.seeCurrentUrlEquals('https://www.gov.uk/check-tenant-right-to-rent-documents');
 });
